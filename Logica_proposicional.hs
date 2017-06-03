8e97ea2d1219cfee1b33f686ad9e9715afc2d79d
Ator96 <oscargutcas9705@gmail.com>
2017-06-02 19:08:10 -0500

    Falta resolver tablas con iff y crear las funciones de Contingencia y contraficcion

--- Logica_proposicional.hs
+++ Logica_proposicional.hs
@@ -1,24 +1,61 @@
 type PropAtom = String
 data Proposicion = Atom PropAtom
-                 | Not Proposicion
-                 | Proposicion `And` Proposicion
-                 | Proposicion `Or`  Proposicion
-                 | Proposicion `Imp` Proposicion
-                 | Proposicion `Iff` Proposicion
-
+		| Proposicion
+                | Not Proposicion
+                | Proposicion `And` Proposicion --And--
+                | Proposicion `Or`  Proposicion --Or--
+                | Proposicion `Imp` Proposicion --Implicacion--
+              	| Proposicion `Iff` Proposicion --Si y solo si--
+		deriving Show
 p = Atom "p"
 q = Atom "q"
 r = Atom "r"
+prop1 :: Proposicion
 prop1 = (p `And` q) `Imp` r
+prop3 = (p `Imp` (p `And` q))
 
-type Valuacion = [(PropAtom, Bool)] 
 e = [("p", True), ("q", True), ("r", False)]
+
+
+--Funcion evaluadora
+type Valuacion = [(PropAtom, Bool)] 
 eval :: Valuacion -> Proposicion -> Bool
-eval _ (PropAtom b) = b
-eval i (Atom  x)    = busca x i 
-eval i (Neg p)      = not (valor i p) 
-eval i (q `And` p ) = valor i p && valor i q
-eval i (q `Imp` p ) = valor i p <= valor i q
-eval i (q `Or` p ) = valor i p || valor i q
+eval i (Atom  x)    	= busca x i 
+eval i (Not p)      	= not (eval i p) 
+eval i (p `And` q ) 	= eval i p && eval i q
+eval i (p `Imp` q ) 	= eval i p <= eval i q
+eval i (p `Or` q ) 	= eval i p || eval i q
+
+busca :: Eq c => c -> [(c,v)] -> v
+busca c t = head [v | (c',v) <- t, c == c']
+
+
+--Funcion encargada de obtener las propocisiones que tenemos
+variables :: Proposicion -> [PropAtom]
+variables (Atom x) 		= [x]
+variables (Not p )		= variables p
+variables (p `And` q ) 	= variables p ++ variables q
+variables (p `Imp` q ) 	= variables p ++ variables q
+
+
+--Funcion encargada de generar los valores booleanos de 2^n 
+--n:= no_argumentos
+interpretacionesVar :: Int -> [[Bool]]
+interpretacionesVar 0  = [[]]
+interpretacionesVar n  = 
+	map (False:) bss ++ map (True:) bss
+	where bss = interpretacionesVar (n -1)
+
+
+--Funcion que combina interpretaciones y a varaibles 
+interpretaciones :: Proposicion -> [Valuacion]
+interpretaciones p =
+	map (zip vs) (interpretacionesVar (length vs))
+	where vs = (variables p)
+
+esTautologia :: Proposicion -> Bool
+esTautologia p =
+	and [eval i p | i <- interpretaciones p]
+
 
 
